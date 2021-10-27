5d86fe1c75b43f792a749a2dfa34989ecb5bef12
Jagan Thathagari <jagan.thathagari@simprogroup.com>
2021-10-27 11:28:01 +1000

    camera

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -298,11 +298,15 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
         intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
 
         if (this.cordova != null) {
-            try {
+            // Let's check to make sure the camera is actually installed. (Legacy Nexus 7 code)
+            PackageManager mPm = this.cordova.getActivity().getPackageManager();
+            if(intent.resolveActivity(mPm) != null)
+            {
                 this.cordova.startActivityForResult((CordovaPlugin) this, intent, (CAMERA + 1) * 16 + returnType + 1);
-            } catch(ActivityNotFoundException e) {
-                e.printStackTrace();
-                LOG.e(LOG_TAG, "Error: You don't have a default camera.  Your device may not be CTS complaint.");
+            }
+            else
+            {
+                LOG.d(LOG_TAG, "Error: You don't have a default camera.  Your device may not be CTS complaint.");
             }
         }
 //        else
